25a17fa674835cf0bf2e16af470c3a5a1536be7b
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-15 16:21:14 +0200

    Use new `EventEmitter` syntax (`.remove()`)

--- example/src/hooks/useIsForeground.ts
+++ example/src/hooks/useIsForeground.ts
@@ -9,8 +9,8 @@ export const useIsForeground = (): boolean => {
     const onChange = (state: AppStateStatus): void => {
       setIsForeground(state === 'active');
     };
-    AppState.addEventListener('change', onChange);
-    return () => AppState.removeEventListener('change', onChange);
+    const listener = AppState.addEventListener('change', onChange);
+    return () => listener.remove();
   }, [setIsForeground]);
 
   return isForeground;
